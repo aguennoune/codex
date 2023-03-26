84989094d20794f12bf2059fefeed8c242a9b2b0
Guennoune Abderrahim <aguennoune@outlook.com>
2023-03-26 02:41:17 +0000

    Changes added to server.js file

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.post('/', async (req, res) => {
     // });
     const response = await openai.createCompletion({
       model: "code-davinci-002",
-      prompt: "\"\"\"\nUtil exposes the following:\nutil.openai() -> authenticates & returns the openai module, which has the following functions:\nopenai.Completion.create(\n    prompt=\"<my prompt>\", # The prompt to start completing from\n    max_tokens=123, # The max number of tokens to generate\n    temperature=1.0 # A measure of randomness\n    echo=True, # Whether to return the prompt in addition to the generated completion\n)\n\"\"\"\nimport util\n\"\"\"\nCreate an OpenAI completion starting from the prompt \"Once upon an AI\", no more than 5 tokens. Does not include the prompt.\n\"\"\"\n",
+      prompt: `${prompt}`,
       temperature: 0,
       max_tokens: 64,
       top_p: 1.0,
